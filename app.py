9dbfc68b71ff4969183de349151068d6037dc931
JohoTechvivasayi <johotechvivasayi@gmail.com>
2025-09-26 14:30:40 +0530

    first commit1

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello World from Flask!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Change host to 0.0.0.0
