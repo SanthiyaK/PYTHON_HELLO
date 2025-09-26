d8d4316e50502e323543ea7f0e71e4cd8edfd683
SanthiyaK <santhiyasantthosh@gmail.com>
2025-09-26 17:14:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World from Flask!"
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)  # Change host to 0.0.0.0
+print('Hello World')
