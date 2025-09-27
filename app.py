31e77f1e4191ac412711551465e4a4aeb38a9e7d
SanthiyaK <santhiyasantthosh@gmail.com>
2025-09-27 20:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("hello world from srtechops, all is well")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from python!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
